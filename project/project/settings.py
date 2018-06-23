234c10b80369272a295afc0196dd01e09d235106
lawrence-gandhar <lawrence_gandhar@yahoo.com>
2018-06-23 05:48:46 +0530

    added postgres database settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'opms',
+        'USER': 'gv',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
