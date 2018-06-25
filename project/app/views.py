6dfb34ef8e2767cca02c044ac105224038697519
Lawrence <lawrence_gandhar@yahoo.com>
2018-06-26 01:31:29 +0530

    added method for admin location dropdown

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404, HttpResponse
 
 # Create your views here.
+
+def location_selects(request):
+    if request.is_ajax():
+        return HttpResponse('')
