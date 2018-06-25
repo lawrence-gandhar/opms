49ed6fb5ad5ee4297212c846e23bdc1c40831faf
Lawrence <lawrence_gandhar@yahoo.com>
2018-06-26 01:17:37 +0530

    form ordering

--- project/app/static/admin_js/usermodel.js
+++ project/app/static/admin_js/usermodel.js
@@ -4,21 +4,37 @@
     $(document).ready(function() {
        /*
        # Setting field order in the admin user form. 
-       # Since super user designates that this user has all permissions without explicitly assigning them. 
-       # if is_admin is selected, will hide permissions and group dowdowns.
-       # Else if_staff, show permissions and group dowdowns.
        */     
 
-        var user_permissions = '<div class="form-row field-user_permissions">'+$(".field-user_permissions").html()+"</div>";
+        var field_username = '<div class="form-row field-username">'+$(".field-username").html()+"</div>";
+        var field_last_login = '<div class="form-row field-last_login">'+$(".field-last_login").html()+"</div>";
+        var field_user_permissions = '<div class="form-row field-user_permissions">'+$(".field-user_permissions").html()+"</div>";
         var field_groups = '<div class="form-row field-groups">'+$(".field-groups").html()+'</div>';
+        var field_date_joined = '<div class="form-row field-date_joined">'+$(".field-date_joined").html()+'</div>';
+        var field_is_superuser = '<div class="form-row field-is_superuser">'+$(".field-is_superuser").html()+'</div>';
 
+        $(".field-username").remove();
+        $(".field-last_login").remove();
         $(".field-user_permissions").remove();
         $(".field-groups").remove();
+        $(".field-date_joined").remove();
+        $(".field-is_superuser").remove();
 
-        $(".field-is_staff").after(user_permissions).after(field_groups);
+        $(".field-password").before(field_username);
+        $(".field-is_staff").after(field_user_permissions).after(field_groups);
+        $(".field-assigned_to").after(field_date_joined);
+        $(".field-date_joined").after(field_last_login);
+        $(".field-email").after(field_is_superuser);
 
         $(".field-user_permissions").hide();
         $(".field-groups").hide();
+
+
+        /*
+        # Since super user designates that this user has all permissions without explicitly assigning them. 
+        # if is_admin is selected, will hide permissions and group dowdowns.
+        # Else if_staff, show permissions and group dowdowns.
+        */
 
         $('[name="is_staff"]').change(function()
         {
