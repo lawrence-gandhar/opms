950580edce9635afe9684c6d16932e21e2e93c26
Lawrence <lawrence_gandhar@yahoo.com>
2018-06-26 03:07:08 +0530

    new url added

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('location-select/', views.location_selects, name='location-select'),
+    path('department-select/', views.department_selects, name='department-select'),
+    path('designation-select/', views.designation_selects, name='designation-select'),
 ]
