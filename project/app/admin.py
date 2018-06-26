1dce5958b07ba2b675b795a7ed3f97c0024563cc
Lawrence <lawrence_gandhar@yahoo.com>
2018-06-27 03:07:00 +0530

    add form class for password hashing

--- project/app/admin.py
+++ project/app/admin.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import * 
+from .models import *
+from django import forms 
 
 # Register your models here.
 
@@ -24,10 +25,26 @@ class LocationAdmin(admin.ModelAdmin):
 class UsertypeAdmin(admin.ModelAdmin):
     list_display = ('id','name','assigned_to', 'link', 'template_folder', 'access_list', 'status',)
 
+#
+# Create a class for form to handle the hashing of the password
+# If you don't set the password as a hash then you won't be able to authenticate
+#
+
+class MyForm(forms.ModelForm):
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(MyForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
+    form = MyForm	
     pass
-    
+
     class Media:
         js = ('admin_js/usermodel.js',)      
   
