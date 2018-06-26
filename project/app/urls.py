44c526fb5757fe3588397c6b8ab89aa6dd80dab6
Lawrence <lawrence_gandhar@yahoo.com>
2018-06-26 23:09:28 +0530

    add new urls for ajax queries

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('location-select/', views.location_selects, name='location-select'),
     path('department-select/', views.department_selects, name='department-select'),
     path('designation-select/', views.designation_selects, name='designation-select'),
+    path('admin-userform-details/', views.get_admin_userform_details, name='admin-userform-details'),
+    path('admin-userform-list/', views.get_admin_user_list, name='admin-userform-list'),
 ]
