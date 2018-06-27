d5095fe23798fed1a85989454eef1fefca7849ba
Lawrence <lawrence_gandhar@yahoo.com>
2018-06-28 01:25:26 +0530

    changed data type of navbar_template column

--- project/app/models.py
+++ project/app/models.py
@@ -43,7 +43,7 @@ class Usertype(models.Model):
     link = models.CharField(max_length=100, null = True, blank = True, db_index = True,)
     template_folder = models.CharField(max_length=20, null = True, blank = True, db_index = True,)
     use_nav_from_template_folder = models.BooleanField(default = True, db_index = True,)
-    navbar_template = models.TextField(null = True, blank = True, db_index = True,)
+    navbar_template = models.CharField(max_length=250, null = True, blank = True, db_index = True,)
     access = models.ManyToManyField('self', db_index = True, blank = True, symmetrical=False, related_name = 'user_access')
 
     def __str__(self):
