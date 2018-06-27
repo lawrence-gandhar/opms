84f5ecac59bc8747835757b4b78998f7d69c66d2
Lawrence <lawrence_gandhar@yahoo.com>
2018-06-28 01:57:38 +0530

    menu_template filter added

--- project/app/templatetags/userdata.py
+++ project/app/templatetags/userdata.py
@@ -1,8 +1,17 @@
 from django import template
+
+# Django settings from settings.py
+from django.conf import settings
+
+# Import models
 from app.models import *
 
+# use Library
 register = template.Library()
 
+#
+# Tag to return url prefix based on user type
+#
 
 @register.simple_tag
 def current_user_link(value):
@@ -15,3 +24,24 @@ def current_user_link(value):
     except:
         pass
     return link
+
+
+#
+# Filter for returning location of menu/sidebar/navigation template
+#
+
+@register.filter
+def menu_template(value):
+    value = int(value)
+    nav_template = settings.BASE_DIR+"/app/templates/app/"
+
+    try:
+        nav_tmp = Usertype.objects.get(pk = value)
+
+        if nav_tmp.use_nav_from_template_folder:
+            nav_template += nav_tmp.template_folder +"/"+ nav_tmp.navbar_template
+        else:
+            nav_template += nav_tmp.navbar_template   
+    except:
+        pass
+    return nav_template   
