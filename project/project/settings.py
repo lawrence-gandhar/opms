73d237d5406eadbdaba78cf9122e73d9825bc503
lawrence-gandhar <lawrence_gandhar@yahoo.com>
2018-07-01 10:39:40 +0530

    chnages made to db

--- project/project/settings.py
+++ project/project/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -84,6 +85,19 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'opms',
+        'USER': 'gv',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
