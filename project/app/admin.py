431d0af4e12146e67ff2d54e50b947b622e6d597
lawrence-gandhar <lawrence_gandhar@yahoo.com>
2018-07-07 13:54:08 +0530

    changes made to assessment settings admin

--- project/app/admin.py
+++ project/app/admin.py
@@ -64,7 +64,7 @@ class CustomUserAdmin(admin.ModelAdmin):
 
 @admin.register(Assessment_Settings)
 class Assessment_SettingsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'abbr', 'year', 'session', 'status', 'enable_self_assessment_form', 'self_assess_users', 'self_assessment_form_start_date', 'self_assessment_form_end_date', 'enable_assessment_grade_form', 'assess_graders', 'assessment_grade_start_date', 'assessment_grade_end_date', 'enable_assess_parameters', 'parameter_form_users', 'parameter_form_graders', 'parameters_start_date', 'parameters_end_date', 'self_assess_template', 'assess_grade_template', 'parameters_template', )
+    list_display = ('name', 'abbr', 'year', 'session', 'status', 'show_only_in_locations', 'show_only_in_departments', 'enable_self_assessment_form', 'self_assess_users', 'self_assessment_form_start_date', 'self_assessment_form_end_date', 'enable_assessment_grade_form', 'assess_graders', 'assessment_grade_start_date', 'assessment_grade_end_date', 'enable_assess_parameters', 'parameter_form_users', 'parameter_form_graders', 'parameters_start_date', 'parameters_end_date', 'self_assess_template', 'assess_grade_template', 'parameters_template', )
 
     class Media:
         js = ('admin_js/common.js',)
